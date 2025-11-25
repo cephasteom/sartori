1380e169abe83b934dc52025767bd4d8dc884249
Peter Thomas <petethomasmusique@gmail.com>
2025-11-25 11:45:55 +0000

    stream help docs

--- src/docs/index.ts
+++ src/docs/index.ts
@@ -5,13 +5,14 @@ import javascript from 'highlight.js/lib/languages/javascript';
 import typescript from 'highlight.js/lib/languages/typescript';
 
 import Pattern from './Pattern.json'
+import Stream from './Stream.json'
 import './style.css';
 
 hljs.registerLanguage('typescript', typescript);
 hljs.registerLanguage('javascript', javascript);
 
 // extract all Pattern methods
-const patternMethods = (Pattern.children.find((item) => item.name === 'methods')?.type?.declaration?.children || [])
+const patternMethods: Record<string, any> = (Pattern.children.find((item) => item.name === 'methods')?.type?.declaration?.children || [])
     .reduce((obj, item) => ({
         ...obj,
         [item.name]: {
@@ -24,27 +25,62 @@ const patternMethods = (Pattern.children.find((item) => item.name === 'methods')
         }
     }), {} as Record<string, any>);
 
-console.log(patternMethods);
+const streamMethods: Record<string, any> = (Stream.children[0]?.children?.filter((item) => ['set'].includes(item.name)) || [])
+    .reduce((obj, item) => ({
+        ...obj,
+        [item.name]:
+        {
+            // @ts-ignore
+            description: (item.signatures[0]?.comment?.summary || [])
+                // @ts-ignore
+                .filter((comment) => comment.kind === 'text')
+                // @ts-ignore
+                .reduce((desc, comment) => desc + comment.text, ''),
+            // @ts-ignore
+            examples: (item.signatures[0]?.comment?.blockTags || [])
+                // @ts-ignore
+                .filter((example) => example.tag === '@example')
+                // @ts-ignore
+                .map((example) => example.content[0]?.text || '')
+        }
+    }), {} as Record<string, any>);
 
 // get element with id 'help
 const helpElement = document.getElementById('help');
 
-// fill with pattern methods
-if (helpElement) {
-    helpElement.innerHTML = `
-        <h2>Pattern</h2>
-        <ul class="help__list">
-            ${Object.entries(patternMethods).map(([name, info]) => `
-                <li>
-                    <h3>${name}</h3>
-                    <p>${info.description}</p>
-                    ${info.examples.length > 0 ? `
-                        ${marked(info.examples.join('\n'))}
-                    ` : ''}
-                </li>
-            `).join('')}
-        </ul>
-    `;
-}
+const renderDocs = (streamMethods: Record<string, any>, patternMethods: Record<string, any>) => {
+    // fill with pattern methods
+    if (helpElement) {
+        helpElement.innerHTML = `
+            <h2>Stream</h2>
+            <ul class="help__list">
+                ${Object.entries(streamMethods).map(([name, info]) => `
+                    <li>
+                        <h3>${name}</h3>
+                        <p>${info.description}</p>
+                        ${info.examples.length > 0 ? `
+                            ${marked(info.examples.join('\n'))}
+                        ` : ''}
+                    </li>
+                `).join('')}
+            </ul>
+            <h2>Pattern</h2>
+            <ul class="help__list">
+                ${Object.entries(patternMethods).map(([name, info]) => `
+                    <li>
+                        <h3>${name}</h3>
+                        <p>${info.description}</p>
+                        ${info.examples.length > 0 ? `
+                            ${marked(info.examples.join('\n'))}
+                        ` : ''}
+                    </li>
+                `).join('')}
+            </ul>
+        `;
+    }
+    hljs.highlightAll();
+};
 
-hljs.highlightAll();
+renderDocs(streamMethods, patternMethods);
+
+// TODO: search box to filter methods
